d7881d78628e93577ca7594cb7b1eb0d1a087b96
Lim Jun Xian <eldrethlim@gmail.com>
2016-09-07 21:53:42 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    'webpack-hot-middleware/client',
+    './app/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\/.js$/|/\.jsx$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'app')
+      },
+      {
+        test: /\.styl$/,
+        loaders: ['style', 'css', 'stylus']
+      }
+    ]
+  },
+  stylus: {
+    use: [require('nib')()],
+    import: ['~nib/lib/nib/index.styl']
+  }
+}
